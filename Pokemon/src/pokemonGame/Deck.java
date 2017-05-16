b67183252e11329eea4eb0ef657712c479b558a9
Joe <jrobertson@students.stab.org>
2017-05-16 00:26:51 -0400

    6th try please work dear Lord

--- Pokemon/src/pokemonGame/Deck.java
+++ Pokemon/src/pokemonGame/Deck.java
@@ -1,6 +1,29 @@
 package pokemonGame;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Deck {
 	
-
+	
+	
+	public static void Draw_from_deck(String args[]){
+		
+		/* READ IN TEXT FILE HERE, WE'LL CALL IT "cards" */
+		LinkedList <String> card_list = new LinkedList<String>();
+		
+		/*Shuffles the list*/
+		Collections.shuffle(card_list);
+		
+		/*Adds all cards to the card_list*/
+		for (int i = 0; i < cards; i++) {
+			card_list.add (new Integer (cards[i]));
+		}
+		
+		/*Same as a queue*/
+		while (!card_list.isEmpty())
+			System.out.println(card_list.removeFirst());
+		System.out.println();
+	}
 }
